262c3a4ee3e4aaee583e087a68f6ff23a9c5e370
Kaz <kazhian@gmail..com>
2024-02-10 23:21:33 +0530

    exercise #1 completed

--- src/main/java/io/javabrains/reactiveworkshop/Exercise1.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise1.java
@@ -1,5 +1,7 @@
 package io.javabrains.reactiveworkshop;
 
+import java.util.stream.Stream;
+
 public class Exercise1 {
 
     public static void main(String[] args) {
@@ -7,24 +9,44 @@ public class Exercise1 {
         // Use StreamSources.intNumbersStream() and StreamSources.userStream()
 
         // Print all numbers in the intNumbersStream stream
-        // TODO: Write code here
+//        StreamSources.intNumbersStream().forEach(System.out::println);
+        printStream(StreamSources.intNumbersStream());
 
         // Print numbers from intNumbersStream that are less than 5
-        // TODO: Write code here
+//        StreamSources.intNumbersStream().filter(n -> n < 5).forEach(System.out::println);
+        printStream(StreamSources.intNumbersStream().filter(n -> n < 5));
 
         // Print the second and third numbers in intNumbersStream that's greater than 5
-        // TODO: Write code here
+        printStream(StreamSources.intNumbersStream().filter(n -> n > 5)
+                .skip(1)
+                .limit(2));
 
         //  Print the first number in intNumbersStream that's greater than 5.
         //  If nothing is found, print -1
-        // TODO: Write code here
+        printStream(StreamSources.intNumbersStream()
+                .filter(n -> n > 5)
+                .findFirst()
+                .orElse(-1)
+        );
 
         // Print first names of all users in userStream
-        // TODO: Write code here
+        printStream(StreamSources.userStream().map(user -> user.getFirstName()));
 
         // Print first names in userStream for users that have IDs from number stream
-        // TODO: Write code here
+        printStream(StreamSources.intNumbersStream()
+                .flatMap(id -> StreamSources.userStream().filter(user -> user.getId() == id))
+                .map(user -> String.format("%s %s", user.getFirstName(), user.getLastName())));
 
+    }
+
+    private static void printStream(Stream s) {
+        System.out.println("-*-*-*-*-*-*-*-*-*-");
+        s.forEach(System.out::println);
+    }
+
+    private static void printStream(int s) {
+        System.out.println("-*-*-*-*-*-*-*-*-*-");
+        System.out.println(s);
     }
 
 }
